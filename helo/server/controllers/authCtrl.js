0285df8245755ae7b3be0a717aa88ab394c6e81a
Owen Ekhator <owenekhator@rocketmail.com>
2018-07-03 16:41:47 -0500

    changed post request to get

--- helo/server/controllers/authCtrl.js
+++ helo/server/controllers/authCtrl.js
@@ -2,19 +2,20 @@ const saveUser =(req,res)=>{
    const { body } = req;
   
     req.app.get('db').auth
-            .save_user([body.username,body.password])
+            .save_user([body.username,body.password,body.profile_pic])
                 .then(response=>{
-                        res.status(200).json('hi')
+                        res.status(200).json(response)
                 })
 }
 
 const login = (req,res) =>{
     
-    const { body } = req;
+    const { params } = req;
 
     req.app.get('db').auth
-            .get_user([body.username,body.password])
+            .get_user([params.username,params.password])
                 .then(response=>{
+                    console.log(response)
                     res.status(200).json(response)
                 })
 }
